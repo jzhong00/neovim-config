38494083ab7b9de94d1c60a8a5241f2bf33cc3da
Jason Zhong <jasonyzhong06@gmail.com>
2024-07-13 23:02:29 +1000

    Removed Ruff

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,57 +2,26 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps hereby
 -- Automatically enable diagnostics when entering a buffer
-vim.cmd([[
-  autocmd BufEnter * lua enable_diagnostics()  -- Enable diagnostics on buffer enter
-]])
 
--- Variable to track warning visibility
-vim.g.show_warnings = true
+local lsp = "pyright" -- Define your language server here
 
--- Function to enable diagnostics display
-function _G.enable_diagnostics()
-  vim.diagnostic.enable()
-  -- Set diagnostic display options to always show errors
-  vim.diagnostic.config({
-    virtual_text = {
-      severity = { min = vim.diagnostic.severity.ERROR },
-    },
-    signs = {
-      severity = { min = vim.diagnostic.severity.ERROR },
-    },
-    underline = {
-      severity = { min = vim.diagnostic.severity.ERROR },
-    },
-  })
-
-  -- If warnings are enabled, include them
-  if vim.g.show_warnings then
-    vim.diagnostic.config({
-      virtual_text = {
-        severity = { min = vim.diagnostic.severity.WARN },
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      pyright = {
+        enabled = lsp == "pyright",
       },
-      signs = {
-        severity = { min = vim.diagnostic.severity.WARN },
+      basedpyright = {
+        enabled = lsp == "basedpyright",
       },
-      underline = {
-        severity = { min = vim.diagnostic.severity.WARN },
+      [lsp] = {
+        enabled = true,
       },
-    })
-  end
-end
-
--- Function to toggle warnings display
-function _G.toggle_warnings()
-  vim.g.show_warnings = not vim.g.show_warnings
-  enable_diagnostics() -- Re-enable diagnostics to apply changes
-end
-
--- Function to disable diagnostics display
-function _G.disable_diagnostics()
-  vim.diagnostic.enable(false)
-end
-
--- Key mapping to toggle warnings display
-vim.api.nvim_set_keymap("n", "<leader>tw", ":lua toggle_warnings()<CR>", { noremap = true, silent = true })
--- Key mapping to disable diagnostics display
-vim.api.nvim_set_keymap("n", "<leader>tf", ":lua disable_diagnostics()<CR>", { noremap = true, silent = true })
+      -- Removed ruff_lsp and ruff configurations
+    },
+    setup = {
+      -- Removed ruff setup
+    },
+  },
+}
